af30bd820c7960f0ea958da8aba8e3169af782d6
Mason Wang <ycda@pxe.csail.mit.edu>
2025-03-16 13:27:03 -0400

    turned into package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cocola",
+    version="0.1.0",
+    description="Cocola package for feature extraction and evaluation",
+    author="Gladia Research Group, Ruben Ciranni    ",
+    author_email="contact@gladia.org",
+    packages=find_packages(),
+    install_requires=[
+        "lightning[pytorch-extra]==2.2.1",
+        "numpy==1.26.4",
+        "torch==2.2.2",
+        "torchvision==0.17.2",
+        "torchaudio==2.2.2",
+        "pandas==2.2.1",
+        "efficientnet_pytorch==0.7.1",
+        "tqdm==4.66.2",
+        "librosa==0.10.1",
+        "soundfile==0.12.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
